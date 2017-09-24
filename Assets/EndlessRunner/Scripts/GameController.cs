dd1288d2760ff28b2d496b593b6bf11111536243
OlavDeng2 <olavkruithof@gmail.com>
2017-09-24 12:54:48 +0200

    Enemy now starts spawning

--- Assets/EndlessRunner/Scripts/GameController.cs
+++ Assets/EndlessRunner/Scripts/GameController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject enemy;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(Spawn(enemy));
+    }
+
+    // Update is called once per fixed unit of time
+    void FixedUpdate ()
+    {
+    }
+
+    IEnumerator Spawn(GameObject Spawnable)
+    {
+        while(true)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(0, 3), 0, 0);
+
+            Quaternion spawnRotation = Quaternion.identity;
+
+            Instantiate(Spawnable, spawnPosition, spawnRotation);
+
+            yield return new WaitForSeconds(Random.Range(1f, 2f));
+        }
+
+    }
 }
