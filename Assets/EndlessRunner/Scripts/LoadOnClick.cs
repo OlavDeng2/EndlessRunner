c2cfff6aa2adac3bf1351275636d3043fd74ad52
OlavDeng2 <olavkruithof@gmail.com>
2017-09-24 12:40:44 +0200

    added functionality to main menu

---
+++ Assets/EndlessRunner/Scripts/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+//Load the next scene
+public class LoadOnClick : MonoBehaviour {
+	
+	//public GameObject loadingImage;
+	
+	public void LoadScene(int Scene)
+	{
+		//loadingImage.SetActive(true);
+		SceneManager.LoadScene(Scene);
+	}
+}
