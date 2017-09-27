6bd59343f50bbb1cb58cf25a9f8a4a29a8ea4de7
OlavDeng2 <olavkruithof@gmail.com>
2017-09-27 11:24:33 +0200

    Updated the GameController script and added some comments for clarity

--- Assets/EndlessRunner/Scripts/GameController.cs
+++ Assets/EndlessRunner/Scripts/GameController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject Enemy;
-    public GameObject PickUp;
+    //variables needed for spawning enemies as well as defining the lanes
+    public GameObject Enemy = null;
+    public GameObject PickUp = null;
     public int LaneDistance = 5;
+    public int DestroyTime = 20;
 
+    //initialize the score
     public int Score = 0;
 
     // Use this for initialization
     void Start ()
     {
+        //start spawning the objects needed for the level
         StartCoroutine(Spawn());
 
     }
@@ -21,18 +25,24 @@ public class GameController : MonoBehaviour {
     {
         while (true)
         {
+            //get the position for where the enemies or pickups will spawn
             Vector3 spawnPosition = new Vector3(Random.Range(-1, 2) * LaneDistance, 1, 1);
-
+            // get the rotation for the objects
             Quaternion spawnRotation = Quaternion.identity;
+
+            //random number to determine whether to spawn enemy or pickup then spawn them
             if( Random.Range(0, 2) == 0)
             {
-                Instantiate(Enemy, spawnPosition, spawnRotation);
+                GameObject EnemyObject = Instantiate(Enemy, spawnPosition, spawnRotation);
+                Destroy(EnemyObject, DestroyTime);
             }
             else
             {
-                Instantiate(PickUp, spawnPosition, spawnRotation);
+                GameObject PickUpObject = Instantiate(PickUp, spawnPosition, spawnRotation);
+                Destroy(PickUpObject, DestroyTime);
             }
 
+            //wait a random amount of time before spawning the next object
             yield return new WaitForSeconds(Random.Range(1f, 2f));
         }
 
